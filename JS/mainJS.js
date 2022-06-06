528d3483094131fa3b6dc1c9e52b1158f389f8aa
Lam <endweed0@gmail.com>
2022-06-06 21:11:15 +0700

    done

--- JS/mainJS.js
+++ JS/mainJS.js
@@ -58,42 +58,78 @@
 
 //*some
 //**Kiem tra phan tu thoa man dieu kien (1 -> true) */
+
 {
-//   var courses = [
-//     {
-//       name: "javascrip",
-//       coin: 680,
-//       isFinish: false,
-//     },
-//     {
-//       name: "php",
-//       coin: 880,
-//       isFinish: false,
-//     },
-//     {
-//       name: "ruby",
-//       coin: 980,
-//       isFinish: true,
-//     },
-//   ];
+  //   var courses = [
+  //     {
+  //       name: "javascrip",
+  //       coin: 680,
+  //       isFinish: false,
+  //     },
+  //     {
+  //       name: "php",
+  //       coin: 880,
+  //       isFinish: false,
+  //     },
+  //     {
+  //       name: "ruby",
+  //       coin: 980,
+  //       isFinish: true,
+  //     },
+  //   ];
+  //   Array.prototype.some2 = function(callback){
+  //     for( var index in this ){
+  //         if ( this.hasOwnProperty(index)){
+  //             if (callback(this[index], index, this) == true){
+  //                 return true;
+  //             }
+  //         }
+  //     }
+  //     return false;
+  //   }
+  //   var result = courses.some2(function(course, index, array){
+  //       return course.isFinish;
+  //   })
+  //   console.log(result);
+}
 
-//   Array.prototype.some2 = function(callback){
-//     for( var index in this ){
-//         if ( this.hasOwnProperty(index)){
-            
-//             if (callback(this[index], index, this) == true){
-//                 return true;
-//             }
-//         }
-//     }
-//     return false;
+//*every
+//**Kiem tra tat ca phan tu thoa man dieu kien (all[true] -> true) */
 
-//   }
-
-
-//   var result = courses.some2(function(course, index, array){
-//       return course.isFinish;
-//   })
-
-//   console.log(result);
+{
+  // var courses = [
+  //   {
+  //     name: "javascrip",
+  //     coin: 680,
+  //     isFinish: true,
+  //   },
+  //   {
+  //     name: "php",
+  //     coin: 880,
+  //     isFinish: true,
+  //   },
+  //   {
+  //     name: "ruby",
+  //     coin: 980,
+  //     isFinish: false,
+  //   },
+  // ];
+  // Array.prototype.every2 = function(callback){
+  //   var outPut = true;
+  //   for(var index in this){
+  //     if(this.hasOwnProperty(index)){
+  //       var result = callback(this[index], index, this);
+  //       if(!result){
+  //         outPut = false;
+  //         break;
+  //       }
+  //     }
+  //   }
+  //   return outPut;
+  // }
+  // var result = courses.every2(function(course, index, array){
+  //     // return course.isFinish;
+  //     return course.coin < 1800;
+  // })
+  // console.log(result);
 }
